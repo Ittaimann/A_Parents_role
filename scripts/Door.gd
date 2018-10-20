e56fafac1fdafd34d3edcd5963aaceb2bd92a683
Joon <j00nj0321@gmail.com>
2018-10-20 16:12:59 -0700

    working on triggering fadeout

--- scripts/Door.gd
+++ scripts/Door.gd
@@ -1,8 +1,21 @@
 extends Area2D
 
+var choseDoor
+
+func _ready():
+	choseDoor = false
+
 func _physics_process(delta):
 	var bodies = get_overlapping_bodies()
 	for body in bodies:
-		if body.name == "Dad":
-			if Input.is_action_pressed("ui_up"):
-				get_tree().change_scene("FadeScene.tscn")
+		if body.name == "DadMoving":
+			print("touching dad")
+			if Input.is_action_pressed("ui_down"):
+				print("down pressed")
+				choseDoor = true
+
+func dad_chose_door():
+	return choseDoor
+	
+	
+	
