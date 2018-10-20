4d38e344454b5193ff1fc7a20d39aaf621d944d5
Ittaimann <ittaiam@gmail.com>
2018-10-20 14:14:15 -0700

    chaning the dialogue and creating dialogue to paly with

--- scenes/jsonTest.gd
+++ scenes/jsonTest.gd
@@ -1,13 +1,21 @@
-extends Label
+extends Node
 
-var dict = {"testing": {"lol":"nani"}}
+var dict = {} 
+var days = 1 
+
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
 func _ready():
 	var file = File.new()
-	file.open("res://generated.json", file.READ)
+	file.open("res://jsontext.json", file.READ)
 	var text = JSON.parse(file.get_as_text())
-	print (text.result)
 	file.close()
+	dict = (text.result)
+
+func _process(delta):
+	print(Input.is_action_just_pressed(("ui_up")))
+	if(Input.is_action_just_pressed("ui_up")):
+		$text.set_text(dict[str(days)][0])
+		days+=1
 # print something from the dictionnary for testing.
