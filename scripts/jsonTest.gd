89f62a6d6f5cf5bd7d2545d7e72232d3c3bee65a
Ittaimann <ittaiam@gmail.com>
2018-10-20 15:58:03 -0700

    additional changes to the ui system

--- scripts/jsonTest.gd
+++ scripts/jsonTest.gd
@@ -17,6 +17,7 @@ func _process(delta):
 	
 	if(Input.is_action_just_pressed("ui_up")):
 		my_node.set_text(dict[str(days)][0])
-		print(my_node.get_text())
+		if(my_node.is_clipping_text()):
+			print(my_node.get_text())
 		days+=1
 # print something from the dictionnary for testing.
