a187a1ad77bf7b5bc81c1b0bc0556c685252ea49
Joon <j00nj0321@gmail.com>
2018-10-21 11:13:49 -0700

    downbutton spam fixed

--- scripts/Door.gd
+++ scripts/Door.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 signal choseDoor
+var paused = false
 
 func _ready():
 	pass
@@ -10,8 +11,17 @@ func _physics_process(delta):
 	var bodies = get_overlapping_bodies()
 	for body in bodies:
 		if body.name == "Dad":
-			if Input.is_action_just_pressed("ui_down"):
+			if not paused and Input.is_action_just_pressed("ui_down"):
 				print("down pressed")
 				emit_signal("choseDoor")
 
 
+
+
+func _on_Door_choseDoor():
+	paused = true
+
+
+
+func _on_Fadeout_nextDay():
+	paused = false
