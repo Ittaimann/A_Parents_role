f42eeb8a45a3bf5a1aecdc8be611d702a2239862
Ittaimann <ittaiam@gmail.com>
2018-10-21 13:28:01 -0700

    global changes

--- global.gd
+++ global.gd
@@ -6,9 +6,10 @@ var daughterTime=0
 func _process(delta):
 	if (days >= 19):
 		if(daughterTime>15):
-			print("good")
+			get_tree().change_scene("res://scenes/goodEnding.tscn")
+
 		#	print("bad end")
 		else:
-			print("bad")
+			get_tree().change_scene("res://scenes/badEnd.tscn")
 		#	print ("good end")
  
