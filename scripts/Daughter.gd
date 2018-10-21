761a422805a9282dfb86be2b1aa1a61b98853cf3
Joon <j00nj0321@gmail.com>
2018-10-21 15:33:24 -0700

    added hide and seek day

--- scripts/Daughter.gd
+++ scripts/Daughter.gd
@@ -2,20 +2,23 @@ extends Area2D
 
 signal talked
 var paused = false
+var dead = false
+var checked = false
 
 func _ready():
 	pass
-	#$daughterPauseTimer.set_wait_time(1)
 	
 
 func _physics_process(delta):
 	if global.daughterTime < 2:
-		self.hide()
+		dead = true
+	
+	if global.days == 4 and not checked:
+		$AnimatedSprite.hide()
 	
 	var bodies = get_overlapping_bodies()
 	for body in bodies:
 		if body.name == "Dad":
-			print("touching")
 			if not paused and Input.is_action_just_pressed("ui_down"):
 				emit_signal("talked")
 	
@@ -24,7 +27,9 @@ func _physics_process(delta):
 
 func _on_text_box_sceneVars(anims):
 	
-	if(anims == "sick"):
+	if anims == "sick":
+		$AnimatedSprite.hide()
+	elif dead:
 		$AnimatedSprite.hide()
 	else:
 		$AnimatedSprite.show()
@@ -32,7 +37,9 @@ func _on_text_box_sceneVars(anims):
 
 
 func _on_Daughter_talked():
-	
+	if global.days == 4:
+		checked = true
+		$AnimatedSprite.show()
 	if global.daughterTime > 1 and not global.days == 7:
 		global.daughterTime = min(global.daughterTime + 1, 20)
 	paused = true
@@ -46,3 +53,5 @@ func _on_Fadeout_nextDay():
 func _on_daughterPauseTimer_timeout():
 	$daughterPauseTimer.stop()
 	paused = false
+
+
