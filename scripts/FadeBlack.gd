8bb9d745904b6df0ccd1e8295de7e39876819da8
Joon <j00nj0321@gmail.com>
2018-10-21 12:43:01 -0700

    polished game start transition

--- scripts/FadeBlack.gd
+++ scripts/FadeBlack.gd
@@ -18,9 +18,9 @@ func _on_Button_pressed():
 
 func _on_fadeTimer_timeout():
 	#emit_signal("veryFirstDay")
-	get_tree().change_scene_to(home)
 	$fadeTimer.set_wait_time(1)
 	$fadeTimer.stop()
+	get_tree().change_scene_to(home)
 
 
 
