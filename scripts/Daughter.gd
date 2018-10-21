8aca1162ea67099dd29d81f91f2760de296ec0bb
Joon <j00nj0321@gmail.com>
2018-10-21 13:22:43 -0700

    adjusted collision box

--- scripts/Daughter.gd
+++ scripts/Daughter.gd
@@ -3,10 +3,15 @@ extends Area2D
 signal talked
 var paused = false
 
+func _ready():
+	$daughterPauseTimer.set_wait_time(1)
+	
+
 func _physics_process(delta):
 	var bodies = get_overlapping_bodies()
 	for body in bodies:
 		if body.name == "Dad":
+			print("touching")
 			if not paused and Input.is_action_just_pressed("ui_down"):
 				emit_signal("talked")
 				var player = AudioStreamPlayer.new()
@@ -28,6 +33,11 @@ func _on_Daughter_talked():
 	paused = true
 
 
-
 func _on_Fadeout_nextDay():
+	$daughterPauseTimer.set_wait_time(2.5)
+	$daughterPauseTimer.start()
+
+
+func _on_daughterPauseTimer_timeout():
+	$daughterPauseTimer.stop()
 	paused = false
