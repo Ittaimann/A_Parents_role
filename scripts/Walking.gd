e1b5ddbd035e4c4ef83812938b884e487b6bc818
Joon <j00nj0321@gmail.com>
2018-10-21 11:38:38 -0700

    forgot to add Walking script

--- scripts/Walking.gd
+++ scripts/Walking.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 
-const GRAVITY = 20
-const SPEED = 175
+#const GRAVITY = 20
+#const SPEED = 175
 
 const GRAVITY = 0
 const SPEED = 300
@@ -10,18 +10,20 @@ const JUMPHEIGHT = -50
 const UP = Vector2(0,-1)
 var motion = Vector2()
 var initialPos = Vector2()
+var paused = false
 
 func _ready():
 	initialPos = transform
+	$dadTimer.set_wait_time(1)
 
 func _physics_process(delta):
 	motion.y += GRAVITY
 
-	if Input.is_action_pressed("ui_right"):
+	if not paused and Input.is_action_pressed("ui_right"):
 		$AnimatedSprite.play("walk")
 		$AnimatedSprite.set_flip_h(false)
 		motion.x = SPEED
-	elif Input.is_action_pressed("ui_left"):
+	elif not paused and Input.is_action_pressed("ui_left"):
 		$AnimatedSprite.set_flip_h(true)
 		$AnimatedSprite.play("walk")
 		motion.x = -SPEED
@@ -37,3 +39,19 @@ func _physics_process(delta):
 func _on_Fadeout_nextDay():
 	print("LOL")
 	transform = initialPos
+	
+	$dadTimer.set_wait_time(1.5)
+	$dadTimer.start()
+
+
+func _on_Door_choseDoor():
+	paused = true
+
+
+func _on_Daughter_talked():
+	paused = true
+
+
+func _on_dadTimer_timeout():
+	paused = false
+	$dadTimer.stop()
